3d146b103f7267ff83263c4d2593bc49356c0357
Ben Barten <bba@fino.digital>
2021-01-19 21:05:37 +0100

    event emitting

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeCart(id) {
+      const index = this.cart.indexOf(id);
+
+      if (index > -1) {
+        this.cart.splice(index, 1);
+      }
+    },
+  },
+  computed: {
+    cartLength() {
+      return this.cart.length;
+    },
+  },
+});
